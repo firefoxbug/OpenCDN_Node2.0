9eac0764395db3bf318553cb9b28c42a666a9a5a
firefoxbug <wanghuafire@gmail.com>
2013-07-31 15:07:05 +0800

    update uninstall scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# Function : uninstall opencdn node 
 # author : firefoxbug
 # E-Mail : wanghuafire@gmail.com
 # Blog   : www.firefoxbug.net
+
+service nginx stop
+service opencdn stop
 
 OPENCDN_PATH="/usr/local/opencdn"
 NGX_PATH="/usr/local/nginx"
@@ -16,3 +20,5 @@ rm -rf ${NGX_PATH}
 rm -rf ${OPENCDN_PATH}
 rm -f /etc/init.d/opencdn
 rm -f /etc/init.d/nginx
+rm -f /var/run/nginx.pid
+rm -f /var/run/opencdn.pid
